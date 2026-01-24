8d4438dce7958706145c99de167428bb4708ff47
kevin cuadros <kevindavidcuadros@gmail.com>
2026-01-24 13:50:12 -0500

    create scroll month days

---
+++ src/components/calendar/Calendar.tsx
@@ -0,0 +1,66 @@
+import styles from './calendar.module.css'
+import { createSignal, For, onMount, Show } from "solid-js";
+import { currentDayID, currentMonth, currentYear, dayInfo, MONTHS } from "../../utils/calendar.utils";
+
+export const Calendar = () => {
+
+  let refDay: HTMLElement;
+  const [selectedDate, setSelectedDate] = createSignal(currentDayID);
+  
+  onMount(() => {
+    refDay?.scrollIntoView({
+      inline: 'center',
+      behavior: 'instant',
+    })
+  })
+
+  const onSelectDate = ( elementId: string ) => {
+    setSelectedDate(elementId)
+    document.getElementById(elementId)?.scrollIntoView({
+      inline: 'center',
+      behavior: 'smooth'
+    })
+  }
+
+  return (
+    <div class={`${styles.container}`}>
+      <div class={`${styles.yearContainer}`}>
+          <p class={`${styles.year}`}>{ currentYear }</p>
+            <Show 
+              when={selectedDate() !== currentDayID} 
+              fallback={<button class={`${styles.today}`}>Today</button>}
+            >
+              <button onclick={() => onSelectDate(currentDayID)}>Today</button>
+            </Show>
+      </div>
+      <p class={`${styles.month}`}>{ MONTHS[currentMonth] }</p>
+      <div class={`${styles.calendar}`}>
+        <For each={dayInfo()}>
+          {(item) =>
+            <li
+              id={item.id}
+              accessKey={item.id}
+              onclick={() => onSelectDate(item.id)}
+              class={`
+                ${item.isCurrentDate && styles.currentDay} 
+                ${item.id === selectedDate() && !item.isCurrentDate && styles.selected} 
+                ${styles.listItem}
+              `}
+              ref={el => { 
+                if(item.isCurrentDate) refDay = el
+              }}
+            >
+              <span>
+                {item.label.slice(0, 3)}
+              </span>
+              <span>
+                {item.day}
+              </span>
+            </li>
+          }
+        </For>
+      </div>
+    </div>
+
+  );
+}
--- src/utils/calendar.utils.ts
+++ src/utils/calendar.utils.ts
@@ -1,11 +1,24 @@
 
-const months = ['January',' February',' March',' April',' May',' June',' July',' August',' September',' October',' November','December']
+export const MONTHS = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
 
 const DATE_OBJ = new Date();
 
-const currentYear = DATE_OBJ.getFullYear();
-const currentMonth = DATE_OBJ.getMonth();
-const currentDayMonth = DATE_OBJ.getUTCDate();
+export const currentYear = DATE_OBJ.getFullYear();
+export const currentMonth = DATE_OBJ.getMonth();
+export const currentDayMonth = DATE_OBJ.getDate();
 
 export const numberDaysByMonth = () => {
   return new Date(currentYear, currentMonth + 1, 0).getDate()
@@ -16,18 +29,21 @@ export const daysCurrentMonth = () => {
 }
 
 export const weekDay = (day: number) => {
-  return new Date(currentYear, currentMonth, day).toLocaleDateString('es', { weekday: 'long' })
+  return new Date(currentYear, currentMonth, day).toLocaleDateString('en', { weekday: 'long' })
 }
+
+export const currentDayID = `${currentDayMonth}-${currentMonth}-${currentYear}`
 
 export const dayInfo = () => {
   return daysCurrentMonth().map( day => {
     return {
-      year: currentYear,
-      monthIndex: currentMonth,
-      monthLabel: months[currentMonth],
-      isCurrentDate: day == currentDayMonth,
       day,
-      label: weekDay(day)
+      id: `${day}-${currentMonth}-${currentYear}`,
+      isCurrentDate: day == currentDayMonth,
+      label: weekDay(day),
+      monthIndex: currentMonth,
+      monthLabel: MONTHS[currentMonth],
+      year: currentYear,
     }
   })
 }
